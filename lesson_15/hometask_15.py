74da938ee7c07ef552c05f3d46ab521975f2ccdf
Olena Fehetsyn <olena.fehetsyn@medtronic.com>
2026-05-12 11:01:14 +0300

    homework_15, fixed function csv, changed file pathes to the list

--- lesson_15/hometask_15.py
+++ lesson_15/hometask_15.py
@@ -4,30 +4,36 @@ import csv
 
 base_path = Path(r"C:\Users\Olena.Fehetsyn\PycharmProjects\PythonProject\aqa-python\lesson_15\csv_report")
 
-file1 = base_path / "random.csv"
-file2 = base_path / "random-michaels.csv"
-
-rows = []
-header = None
-
-for file in [file1, file2]:
-    with open(file, "r") as f:
-        reader = csv.reader(f)
-        file_header = next(reader)
-
-        if header is None:
-            header = file_header
-
-        for row in reader:
-            rows.append(tuple(row))
-
-unique_rows = set(rows)
-
+csv_files = [
+    base_path / "random.csv",
+    base_path / "random-michaels.csv"
+    ]
 output_file = base_path / "result_fehetsyn.csv"
 
-with open(output_file, "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(header)
-    writer.writerows(unique_rows)
 
+def remove_duplicates(csv_file, output_file):
+    rows = []
+    header = None
+    for file in csv_files:
+        with open(file, "r") as f:
+            reader = csv.reader(f)
+            file_header = next(reader)
+
+            if header is None:
+                header = file_header
+
+            for row in reader:
+             rows.append(tuple(row))
+
+    unique_rows = set(rows)
+
+
+
+    with open(output_file, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerows(unique_rows)
+
+if __name__ == "__main__":
+    remove_duplicates(csv_files, output_file)
 print("completed successfully")
--- lesson_15/hometask_15_2.py
+++ lesson_15/hometask_15_2.py
@@ -4,13 +4,15 @@ import json
 from logger import log_event
 
 base_path = Path(r"C:\Users\Olena.Fehetsyn\PycharmProjects\PythonProject\aqa-python\lesson_15\json_files")
-file_path1 = base_path / "localizations_en.json"
-file_path2 = base_path / "localizations_ru.json"
-file_path3 = base_path / "login.json"
-file_path4 = base_path / "swagger.json"
+files = [
+    base_path / "localizations_en.json",
+    base_path / "localizations_ru.json",
+    base_path / "login.json",
+    base_path / "swagger.json",
+]
 
-def valid_json(file_path1, file_path2, file_path3, file_path4):
-    for file_path in [file_path1, file_path2, file_path3, file_path4]:
+def valid_json(files):
+    for file_path in files:
         try:
             with open(file_path, "r", encoding="utf-8") as f:
                 data = json.load(f)
@@ -20,7 +22,7 @@ def valid_json(file_path1, file_path2, file_path3, file_path4):
             log_event(file_path.name, f"Invalid JSON: {e}")
 
 if __name__ == "__main__":
-    valid_json(file_path1, file_path2, file_path3, file_path4)
+    valid_json(files)
 
 
 
